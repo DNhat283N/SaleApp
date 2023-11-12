eb6d692f6bb6064959f975d03e92280aca7c15e7
DNhat283N <nhat.nd283@gmail.com>
2023-11-12 21:35:22 +0700

    add database

--- app/dao.py
+++ app/dao.py
@@ -1,81 +1,13 @@
+from app.models import Category, Product
+
+
 def get_categories():
-    return [{
-        'id': 1,
-        'name': 'Mobile'
-    }, {
-        'id': 2,
-        'name': 'Tablet'
-    }
-    ]
+    return Category.query.all()
 
 
 def get_products(key):
-    products = [
-        {
-            'id': 1,
-            'name': 'iPhone 13',
-            'price': 20000000,
-            'category_id': 1,
-            'image': "https://cdn1.viettelstore.vn/Images/Product/ProductImage/213191152.jpeg"
-        },
-        {
-            'id': 2,
-            'name': 'iPhone 13 Pro',
-            'price': 23000000,
-            'category_id': 1,
-            'image': "https://cdn2.cellphones.com.vn/insecure/rs:fill:358:358/q:80/plain/https://cellphones.com.vn/media/catalog/product/i/p/iphone-13-pro.png"
-        },
-        {
-            'id': 3,
-            'name': 'iPhone 13 Pro Max',
-            'price': 27000000,
-            'category_id': 1,
-            'image': "https://cdn2.cellphones.com.vn/insecure/rs:fill:358:358/q:80/plain/https://cellphones.com.vn/media/catalog/product/i/p/iphone-13-pro-max-256gb.png"
-        },
-        {
-            'id': 4,
-            'name': 'iPhone 14',
-            'price': 26000000,
-            'category_id': 1,
-            'image': "https://cdn2.cellphones.com.vn/insecure/rs:fill:358:358/q:80/plain/https://cellphones.com.vn/media/catalog/product/i/p/iphone-14_1.png"
-        },
-        {
-            'id': 5,
-            'name': 'iPhone 14 Pro',
-            'price': 26000000,
-            'category_id': 1,
-            'image': "https://cdn2.cellphones.com.vn/insecure/rs:fill:358:358/q:80/plain/https://cellphones.com.vn/media/catalog/product/i/p/iphone-14-pro_2__4.png"
-        },
-        {
-            'id': 6,
-            'name': 'iPhone 14 Pro Max',
-            'price': 26000000,
-            'category_id': 1,
-            'image': "https://cdn2.cellphones.com.vn/insecure/rs:fill:358:358/q:80/plain/https://cellphones.com.vn/media/catalog/product/i/p/iphone-14-pro_2__5.png"
-        },
-        {
-            'id': 7,
-            'name': 'Xiaomi 13',
-            'price': 11750000,
-            'category_id': 1,
-            'image': "https://cdn.mobilecity.vn/mobilecity-vn/images/2022/12/w300/xiaomi-13-xanh-mint-1.png.webp"
-        },
-        {
-            'id': 8,
-            'name': 'Xiaomi 14 Snapdragon 8 Gen 3',
-            'price': 17950000,
-            'category_id': 1,
-            'image': "https://cdn.mobilecity.vn/mobilecity-vn/images/2023/10/w300/xiaomi-14-pro-titanium.jpg.webp"
-        },
-        {
-            'id': 9,
-            'name': 'Xiaomi 13 5G(Dimensity 1080)',
-            'price': 11750000,
-            'category_id': 1,
-            'image': "https://cdn.mobilecity.vn/mobilecity-vn/images/2023/07/w300/xiaomi-redmi-13-ro-ri-00.jpg.webp"
-        },
-    ]
+    products = Product.query
     if key:
-        products = [p for p in products if p['name'].find(key) >= 0]
+        products = products.filter(Product.name.contains(key))
 
-    return products
+    return products.all()
--- app/models.py
+++ app/models.py
@@ -23,14 +23,24 @@ class Product(db.Model):
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    #
-    #     c1 = Category(name='Mobile')
-    #     c2 = Category(name='Tablet')
-    #     c3 = Category(name='Laptop')
-    #
-    #     db.session.add(c1)
-    #     db.session.add(c2)
-    #     db.session.add(c3)
-    #
-    #     db.session.commit()
+
+        c1 = Category(name='Mobile')
+        c2 = Category(name='Tablet')
+        c3 = Category(name='Laptop')
+
+        db.session.add(c1)
+        db.session.add(c2)
+        db.session.add(c3)
+
+        db.session.commit()
+
+        p1 = Product(name='iPhone 13', price=20000000, category_id=1)
+        p2 = Product(name='iPhone 13 Pro', price=23000000, category_id=1)
+        p3 = Product(name='iPhone 13 Pro Max', price=27000000, category_id=1)
+        p4 = Product(name='Galaxy S23', price=18000000, category_id=1)
+        p5 = Product(name='iPad Pro 2023', price=21000000, category_id=2)
+
+        db.session.add_all(p1, p2, p3, p4, p5)
+        db.session.commit()
+
 
